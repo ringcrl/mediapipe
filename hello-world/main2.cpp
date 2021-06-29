b70397f1bba09cca5baa184bdb48dcbfb088feb4
Pinku <prantoran@gmail.com>
2021-06-28 21:54:26 -0700

    push packet to graph

--- hello-world/main2.cpp
+++ hello-world/main2.cpp
@@ -30,8 +30,29 @@ absl::Status PrintHelloWorld() {
 
   mediapipe::CalculatorGraph graph;
   MP_RETURN_IF_ERROR(graph.Initialize(config));
+  ASSIGN_OR_RETURN(mediapipe::OutputStreamPoller poller,
+                   graph.AddOutputStreamPoller("out"));
+
+  MP_RETURN_IF_ERROR(graph.StartRun({}));
+  
+  for (int i = 0; i < 1; ++i) {
+    MP_RETURN_IF_ERROR(graph.AddPacketToInputStream(
+        "in", mediapipe::MakePacket<std::string>("Wasmup!").At(mediapipe::Timestamp(i))));
+  }
+
+  // Close the input stream "in".
+  MP_RETURN_IF_ERROR(graph.CloseInputStream("in"));
+
+  mediapipe::Packet packet;
+  // // Get the output packets std::string.
+  // while (poller.Next(&packet)) {
+  //   LOG(INFO) << packet.Get<std::string>();
+  // }
+
+  return graph.WaitUntilDone();
+
   MP_RETURN_IF_ERROR(absl::InvalidArgumentError("testing MP_RETURN_IF_ERROR macro"));
-  return absl::OkStatus();
+  // return absl::OkStatus();
 }
 
 
